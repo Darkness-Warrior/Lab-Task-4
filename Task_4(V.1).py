31b7f45736fa9b8bac76f6a4608626d6591ef6d1
Egor <123231130+Darkness-Warrior@users.noreply.github.com>
2023-03-20 08:55:42 +0300

    Update Task_4(V.1).py

--- Task_4(V.1).py
+++ Task_4(V.1).py
@@ -1,5 +1,4 @@
 import string
-print(string.ascii_uppercase)
 
 
 class Alphabet:
@@ -8,24 +7,32 @@ class Alphabet:
         self.letters = letters
 
     def print(self):
-        print(self.letters)
+        return self.letters
 
     def letters_num(self):
         return len(self.letters)
 
 
 class EngAlphabet(Alphabet):
-    def __init__ (self):
-        super().__init__('En', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    def __init__(self):
+        super().__init__('En', string.ascii_uppercase)
         self._letters_num = len(self.letters)
 
     def is_en_letter(self, letter):
         return letter in self.letters
 
-    def letters_num (self):
+    def letters_num(self):
         return self._letters_num
 
     @staticmethod
     def primer():
         return "In the last task, I was inspired by Quake 3"
+
+
+eng_alphabet = EngAlphabet()
+print(eng_alphabet.print())
+print(eng_alphabet.letters_num())
+print(eng_alphabet.is_en_letter('F'))
+print(eng_alphabet.is_en_letter('Щ'))
+print(EngAlphabet.primer())
 
